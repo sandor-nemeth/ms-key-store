81d787359c6074ad7bb7eae13d5ba77eb4a97287
Sandor Nemeth <sandor.nemeth.1986@gmail.com>
2016-06-17 10:05:59 +0200

    Service implementation without tests

--- src/main/java/com/github/sandornemeth/App.java
+++ src/main/java/com/github/sandornemeth/App.java
@@ -1,13 +1,76 @@
 package com.github.sandornemeth;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class App {
+    final static String queueName = "kv-store";
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    HelloService helloService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("kv-store-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(
+            ConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container =
+                new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter messageListenerAdapter() {
+        MessageListenerAdapter adapter = new MessageListenerAdapter
+                (helloService, "receiveMessage");
+        adapter.setMessageConverter(new SimpleMessageConverter());
+        return adapter;
     }
 }
---
+++ src/main/java/com/github/sandornemeth/HelloService.java
@@ -0,0 +1,36 @@
+package com.github.sandornemeth;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A simple controller.
+ *
+ * @author sandornemeth
+ */
+@RestController
+public class HelloService {
+
+    @Autowired
+    private StringRedisTemplate redis;
+
+    @RequestMapping(value = "/get/{key}", method = RequestMethod.GET)
+    public String getValue(@PathVariable("key") String key) {
+        return redis.opsForValue().get(key);
+    }
+
+    public void receiveMessage(String message) {
+        String msg = String.valueOf(message);
+        String[] split = msg.split(":");
+        String key = split[0];
+        String value = split.length > 1 ? split[1] : "";
+        redis.opsForValue().set(key, value);
+    }
+
+}
+
