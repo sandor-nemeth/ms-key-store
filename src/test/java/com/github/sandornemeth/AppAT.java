68548bfd383bf2d2f833a4c9d31dcb67098681de
Sandor Nemeth <sandor.nemeth.1986@gmail.com>
2016-06-17 16:34:15 +0200

    Add test configuration
    Initial test configuration for unit, integration and acceptance tests

---
+++ src/test/java/com/github/sandornemeth/AppAT.java
@@ -0,0 +1,15 @@
+package com.github.sandornemeth;
+
+import org.junit.Test;
+
+/**
+ * @author sandornemeth
+ */
+public class AppAT {
+
+    @Test
+    public void acceptanceTest() {
+        System.out.println("acceptance test");
+    }
+
+}
---
+++ src/test/java/com/github/sandornemeth/AppIT.java
@@ -0,0 +1,15 @@
+package com.github.sandornemeth;
+
+import org.junit.Test;
+
+/**
+ * @author sandornemeth
+ */
+public class AppIT {
+
+    @Test
+    public void integrationTest() {
+        // accessing redis
+    }
+
+}
